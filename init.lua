92d6dbee8145c70324b060af9c08a49383efd19c
raymoo <uguu@installgentoo.com>
2016-11-13 01:46:56 -0800

    Finish basic implementation

--- init.lua
+++ init.lua
@@ -1,13 +1,130 @@
 local LIB_PATH = "preempter.so"
+local MAX_FILE_SIZE = 100000
+local MAX_MEMORY = 1024
+
+cloud = {}
+local modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
+cloud.modpath = modpath
 
 local insecure_env = minetest.request_insecure_environment()
 if not insecure_env then
-   local err = "[cloud] This mod requires an insecure environment to run.\n"
-   err = err .. "Please add this mod as a trusted mod, or disable mod security."
-   error(err)
+	local err = "[cloud] This mod requires an insecure environment to run.\n"
+	err = err .. "Please add this mod as a trusted mod, or disable mod security."
+	error(err)
+end
+-- cloud.sandboxed_create = insecure_env.package.loadlib(modpath .. LIB_PATH, "sandboxed_create")
+cloud.sandboxed_resume = insecure_env.package.loadlib(modpath .. LIB_PATH, "sandboxed_resume")
+
+local Computer = dofile(modpath .. "computer.lua")
+local Buffer = dofile(modpath .. "buffer.lua")
+
+local os_file = assert(io.open(modpath .. "os.lua", "r"))
+local os_source = os_file:read("*all")
+os_file:close()
+
+local function env()
+	return {
+		yield = coroutine.yield,
+		table = {
+			concat = table.concat,
+			insert = table.insert,
+		},
+		Buffer = Buffer,
+		ipairs = ipairs,
+		pairs = pairs,
+		string = {
+			sub = string.sub
+		},
+		tostring = tostring,
+	}
 end
 
-local modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
+local function caps(cpu, mem)
+	return { cpu = cpu, memory = mem }
+end
 
-local sandboxed_create = insecure_env.package.loadlib(modpath .. LIB_PATH, "sandboxed_create")
-local sandboxed_resume = insecure_env.package.loadlib(modpath .. LIB_PATH, "sandboxed_resume")
+local function os_program()
+	local program = loadstring(os_source)
+	setfenv(program, env())
+	return program
+end
+
+-- Screen formspecs
+function cloud.screen_formspec(display)
+	local everything = {}
+	everything[1] = "size[8,8]textarea[0,0;8,7;output;;"
+	everything[2] = minetest.formspec_escape(display)
+	everything[3] = "]field[0,7;6,1;input;;]button[7,7;1,1;submit;>]"
+
+	return table.concat(everything)
+end
+
+local hash_pos = minetest.hash_node_position
+local unhash_pos = minetest.get_position_from_hash
+
+-- Map from position hashes to computers
+local active_computers = {}
+local function run_computers()
+	local remove_these = {}
+	for poshash, computer in pairs(active_computers) do
+		local pos = unhash_pos(poshash)
+		local node = minetest.get_node_or_nil(pos)
+		if node and node.name == "cloud:computer" then
+			computer:run(pos)
+			if computer.state ~= "on" then
+				table.insert(remove_these, poshash)
+			end
+		else
+			table.insert(remove_these, poshash)
+		end
+	end
+
+	for i, poshash in ipairs(remove_these) do
+		active_computers[poshash] = nil
+	end
+end
+minetest.register_globalstep(run_computers)
+
+local function add_computer(pos, computer)
+	active_computers[hash_pos(pos)] = computer
+end
+
+local function start_computer(pos, meta)
+	local computer = Computer.new(env(), caps(1000, MAX_MEMORY), os_program())
+	add_computer(pos, computer)
+
+	return computer
+end
+
+function cloud.get_computer(pos)
+	return active_computers[hash_pos(pos)]
+end
+
+-- Computer Nodes
+minetest.register_node("cloud:computer", {
+	description = "Computer",
+	groups = { cracky = 3 },
+	drawtype = "normal",
+	tiles = { "cloud_computer.png" },
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
+		local p_name = placer and placer:get_player_name() or ""
+		local meta = minetest.get_meta(pos)
+		meta:set_string("owner", p_name)
+		meta:set_string("formspec", cloud.screen_formspec(""))
+		meta:set_string("used_space", 0)
+		meta:set_string("max_space", MAX_FILE_SIZE)
+		start_computer(pos, meta)
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		local computer = cloud.get_computer(pos)
+		if not computer then
+			computer = start_computer(pos, minetest.get_meta(pos))
+		elseif fields.submit and fields.input then
+			for s in string.gmatch(fields.input, "([^\n]+)\n?") do
+				computer:interrupt("text", s)
+			end
+			local meta = minetest.get_meta(pos)
+			meta:set_string("formspec", cloud.screen_formspec(fields.output or ""))
+		end
+	end,
+})
